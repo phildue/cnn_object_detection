e2f5f65d8b8de3146e09274d7777eb558ab976bd
phild <phild@protonmail.com>
2019-01-27 09:01:50 +0100

    training with multi outputs working

--- src/python/demo/training.py
+++ src/python/demo/training.py
@@ -21,9 +21,9 @@ anchors = np.array([
     [[10, 13],
      [16, 30],
      [33, 23]],
-    #   [30, 61],
-    #   [62, 45],
-    #   [59, 119],
+    [[30, 61],
+     [62, 45],
+     [59, 119]],
     #   [116, 90],
     #   [156, 198],
     #   [373, 326]
@@ -36,11 +36,11 @@ img_size = (52, 52)
 grids = [
     #   [26, 26],
     [13, 13],
-    #   [7, 7]
+    [7, 7]
 ]
 n_classes = 92
 batch_size = 1
-n_boxes = 3
+n_boxes = [3,3]
 encoder = YoloV3Encoder(anchor_dims=anchors, img_size=img_size, grids=grids, n_classes=n_classes,
                         verbose=0)
 decoder = YoloV3Decoder(img_size=img_size, grids=grids, anchor_dims=anchors, n_classes=n_classes)
@@ -52,14 +52,12 @@ conv1 = Conv2D(filters=4, kernel_size=(3, 3), strides=(2, 2), padding='same')(ne
 act1 = LeakyReLU(0.4)(conv1)
 conv2 = Conv2D(filters=4, kernel_size=(3, 3), strides=(2, 2), padding='same')(conv1)  # 13
 act2 = LeakyReLU(0.4)(conv2)
-out = Conv2D(n_boxes * (n_classes + + 1 + 4), kernel_size=(1, 1))(act2)
-out = Reshape((-1, n_classes + 1 + 4))(out)
-meta_t = K.constant(YoloV3Encoder.generate_encoding_tensor((13, 13), grids, anchors, 4),
-                    K.tf.float32)
+out1 = Conv2D(n_boxes[0] * (n_classes + + 1 + 4), kernel_size=(1, 1))(act2)
+conv3 = Conv2D(filters=4, kernel_size=(3, 3), strides=(2, 2), padding='same')(conv2)  # 13
+act3 = LeakyReLU(0.4)(conv3)
+out2 = Conv2D(n_boxes[1] * (n_classes + + 1 + 4), kernel_size=(1, 1))(act3)
 
-netout = ConcatMeta(meta_t)(out)
-
-network = Model(netin, netout)
+network = Model(netin, [out1, out2])
 optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 
 network.compile(optimizer=optimizer, loss=loss.total_loss)
@@ -68,12 +66,15 @@ network.compile(optimizer=optimizer, loss=loss.total_loss)
 def preprocess(gen):
     for imgs, labels in gen:
         x = np.zeros((batch_size, img_size[0], img_size[1], 3))
-        y = np.zeros((batch_size, 13 * 13 * 3, n_classes + 4 + 1 + 6))
+        y = [np.zeros((batch_size, grid[0], grid[1], n_boxes[ig], n_classes + 4 + 1 + 6)) for ig, grid in
+             enumerate(grids)]
         idx = 0
         for img, label in zip(imgs, labels):
             img_, label_ = resize(img, img_size, label=label)
             x[idx] = img_.array
-            y[idx] = encoder.encode_label(label_)
+            y_outs = encoder.encode_label(label_)
+            for idx_out, y_out in enumerate(y_outs):
+                y[idx_out][idx] = y_out
             idx += 1
         yield x, y
 
